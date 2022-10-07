0ae14a5edb0c2fd23a71109dde0b9d4a05b861bf
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-10-07 12:42:03 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,18 @@
+import { AddAccount } from '../../domain/usecases/add-account'
 import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse, EmailValidator, Controller } from '../protocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
   // Utilizamos a interface EmailValidator para fazer a inversão de dependencia,
   // pois assim podemos trocar a implementação do emailValidator sem precisar alterar o código
   // em um possível refactory
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -27,7 +30,7 @@ export class SignUpController implements Controller {
         }
       }
 
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
 
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
@@ -37,6 +40,12 @@ export class SignUpController implements Controller {
       if (!isValidEmail) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      this.addAccount.add({
+        name,
+        email,
+        password
+      })
     } catch (error) {
       return serverError()
     }
--- src/presentation/protocols/index.ts
+++ src/presentation/protocols/index.ts
@@ -1,3 +1,7 @@
 export * from './controller'
 export * from './http'
 export * from './email-validator'
+
+// Nos protocolos, salvamos as interfaces que são utilizadas em mais de um lugar
+// Que determinam padrões a serem utilizados na aplicação
+// No entanto, não representam useCases (casos-de-uso)
