eadfb2c2c03c706add48f9df4f98aebc4f272b9d
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-10-12 14:56:25 -0300

    feat: ensure SignUp route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setupRoutes from './routes'
 
 const app = express()
 
 // Ao invés de chamar middleware um por um, chamo uma função
 // responsável por fazer o setup dos middlewares
 setupMiddlewares(app)
+
+// Ao invés de chamar rotas um por um, chamo uma função
+setupRoutes(app)
 
 export default app
---
+++ src/main/config/routes.ts
@@ -0,0 +1,26 @@
+import { Express, Router } from 'express'
+
+// Assim como um fs do node, o fastglob nos auxilia a monitorar/ler arquivos dentros de pastas.
+import fg from 'fast-glob'
+
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+
+  // A proposta utilizada aqui é nunca precisar modificar o arquivo de rotas ao criar novas rotas.
+  // O próprio routes.ts saberá que uma rota deve ser adicionada e servida.
+  // Portanto, o que fazemos é ler todos os arquivos com a extensão .routes.ts
+  // e assim, percorrer cada um deles.
+  // Utilizando o map, fazemos uma importação 'diferenciada', pois não é no começo do arquivo
+  // e sim no meio, dentro do map, portanto utilizamos:
+  // 1. O await no import, pois a importação é assincrona
+  // 2. Passamos para o import o path partindo do diretório atual
+  // no entanto como o fg.sync retorna um array de string, no formato 'src/main/...
+  // precisamos voltar para a raíz, partindo do diretório atual.
+  // e após isso concatenamos o path do arquivo que queremos importar.
+  // 3. a notação .default, pega a exportação default do arquivo importado.
+  // e passa a instancia do Routes do express.
+  fg.sync('**/src/main/routes/**routes.ts').map(async file =>
+    (await import (`../../../${file}`)).default(router)
+  )
+}
