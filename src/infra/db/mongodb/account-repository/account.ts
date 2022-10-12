20e80d3bbda2e524f138aafef78d96ad13d66939
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-10-12 12:31:40 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,20 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  // É responsabilidade do nosso repositório fazer o parse do _id do mongo para id
+  // Uma vez que o _id é específico da implementação com o mongo,
+  // sendo assim nossa camada de domínio não deve se adaptar ao mongo
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = {
+      ...accountData,
+      id: String(result.insertedId)
+    }
+
+    return account
+  }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,12 +1,17 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+
   async connect (uri: string): Promise<void> {
-    this.client = MongoClient.connect(uri)
+    this.client = await new MongoClient(uri).connect()
   },
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
