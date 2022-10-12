c743693c20c7c390c360e04bab547d6ab121b075
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-10-12 15:02:06 -0300

    refactor: connect to mongodb before ingration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,9 +1,25 @@
 // Extensão test, serve para teste de integração
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL || '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  // Evita que fique sujo o banco de dados
+  // E interfira em outros testes
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('should return an account on sucess', async () => {
     await request(app)
       .post('/api/signup')
