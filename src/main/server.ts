0f33865c90e0f2b1ed918642cb7aa9760fe9c616
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-10-12 15:19:18 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,11 @@
 // Camada onde criamos as instancias de todos arquivos
 // Onde criamos a árvore de dependencia de cada controlador
 // Define-se por Factory da aplicação.
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-import app from './config/app'
-
-app.listen(3333, () => console.log('Server running at http://localhost:3333'))
+MongoHelper.connect(env.mongoUrl).then(async () => {
+  // Para garantir que o app não servirá nada, enquanto o banco não estiver conectado
+  const app = (await import ('./config/app')).default
+  app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+}).catch(console.error)
